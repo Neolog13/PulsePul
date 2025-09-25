1eab1ae3d4b84af2808c1059161b8011c3c29868
Vladimir Timchenko <neologfly@gmail.com>
2025-09-25 17:33:55 +0300

    Marking for new measurements

---
+++ webapp/src/pages/newMeasurement/index.tsx
@@ -0,0 +1,10 @@
+import css from './index.module.scss'
+
+export const NewMeasPage = () => {
+  return (
+    <div>
+      <h1 className={css.title}>New measurement</h1>
+      <div>Form will be here...</div>
+    </div>
+  )
+}
