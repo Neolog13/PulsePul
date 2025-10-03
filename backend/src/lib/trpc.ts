f049de1ca4b663253f1942bdd2699e764b59ad2f
Vladimir Timchenko <neologfly@gmail.com>
2025-10-03 15:30:41 +0300

    Fix type import in lib/trpc

--- backend/src/lib/trpc.ts
+++ backend/src/lib/trpc.ts
@@ -1,7 +1,7 @@
 import { initTRPC } from '@trpc/server'
 import { type Express } from 'express'
 import * as trpcExpress from '@trpc/server/adapters/express'
-import { TrpcRouter } from '../router'
+import type { TrpcRouter } from '../router'
 
 export const trpc = initTRPC.create()
 export const applyTrpcToExpressApp = (expressApp: Express, trpcRouter: TrpcRouter) => {
