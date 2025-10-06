634fe183d8175b0f4d234e20986cd3a6ba047753
Vladimir Timchenko <neologfly@gmail.com>
2025-10-06 18:19:04 +0300

    Add Input component and styles for it

--- webapp/src/components/Input/index.tsx
+++ webapp/src/components/Input/index.tsx
@@ -1,17 +1,21 @@
 import { type FormikProps } from 'formik'
 import type React from 'react'
+import css from './index.module.scss'
+import cn from 'classnames'
 
 export interface InputProps {
   name: string
   label: string
   type: 'date' | 'time' | 'number'
   formik: FormikProps<any>
+  maxWidth?: number
   onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void
 }
 
-export const Input = ({ name, label, type, formik, onBlur }: InputProps) => {
+export const Input = ({ name, label, type, formik, maxWidth, onBlur }: InputProps) => {
   const value = formik.values[name]
-
+  const disabled = formik.isSubmitting
+  const invalid = formik.errors[name]
   const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
     if (onBlur) {
       onBlur(e)
@@ -20,20 +24,25 @@ export const Input = ({ name, label, type, formik, onBlur }: InputProps) => {
   }
 
   return (
-    <div style={{ marginBottom: 10 }}>
-      <label htmlFor={name}>{label}</label>
-      <br />
+    <div className={cn({ [css.field]: true, [css.disabled]: disabled })}>
+      <label className={css.label} htmlFor={name}>
+        {label}
+      </label>
       <input
+        className={cn({
+          [css.input]: true,
+          [css.invalid]: invalid,
+        })}
+        style={{ maxWidth }}
         type={type}
         onChange={(e) => formik.setFieldValue(name, e.target.value)}
         onBlur={handleBlur}
         value={value}
         name={name}
         id={name}
+        disabled={formik.isSubmitting}
       />
-      {formik.touched[name] && formik.errors[name] && (
-        <div style={{ color: 'red' }}>{formik.errors[name] as string}</div>
-      )}
+      {formik.touched[name] && invalid && <div className={css.error}>{formik.errors[name] as string}</div>}
     </div>
   )
 }
