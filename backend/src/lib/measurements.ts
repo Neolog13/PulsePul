02697c787bb7d73044080b357fffcea0a55888de
Vladimir Timchenko <neologfly@gmail.com>
2025-10-06 18:16:11 +0300

    Chande logic of generated mock date of measurement

--- backend/src/lib/measurements.ts
+++ backend/src/lib/measurements.ts
@@ -1,8 +1,14 @@
 import _ from 'lodash'
 
-export const measurements = _.times(100, (i) => ({
-  date: `date-and-time-${i}`,
-  sap: 'number1',
-  dap: 'number2',
-  pulse: 'number3',
-}))
+export const measurements = _.times(100, (i) => {
+  const date = new Date()
+  date.setDate(date.getDate() - i) // Каждое измерение на день раньше
+  
+  return {
+    date: date.toISOString().split('T')[0],
+    time: `${_.random(8, 20).toString().padStart(2, '0')}:${_.random(0, 59).toString().padStart(2, '0')}`,
+    sap: _.random(100, 180).toString(),
+    dap: _.random(60, 100).toString(), 
+    pulse: _.random(60, 100).toString(),
+  }
+})
